edc452265abaac654c671249ade57bbe9ade5fb2
Michael Hardy <michaelhardyajr@gmail.com>
2020-01-22 18:42:13 -0500

    ip address fix

--- deploy.js
+++ deploy.js
@@ -89,7 +89,7 @@ function sshConnect() {
 	ssh
 		.connect({
 			// TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-			host: "54.164.123.24",
+			host: "3.93.236.208",
 			username: "ubuntu",
 			privateKey: "hs-key.pem"
 		})
